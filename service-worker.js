00c2f982be546e5c22c2df11962a2705a114afee
intrnl <intrnl@users.noreply.github.com>
2020-08-11 03:58:50 +0000

    deploy: 6cfbb256a84dcc48b902303e22f8bd1eb4ebe180

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-!function(){"use strict";self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(async e=>{for(let t of e)1597118123467!==t&&await caches.delete(t);self.clients.claim()}))})}();
+!function(){"use strict";self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(async e=>{for(let t of e)1597118329110!==t&&await caches.delete(t);self.clients.claim()}))})}();
