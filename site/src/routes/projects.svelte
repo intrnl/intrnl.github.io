97d61d9f52c1821e754708e3fd40653b266c1542
intrnl <intrnl28+git@gmail.com>
2020-08-26 08:02:19 +0700

    redo projects page

--- site/src/routes/projects.svelte
+++ site/src/routes/projects.svelte
@@ -1,3 +1,82 @@
+<script context='module'>
+	let entries = [
+		{
+			header: 'Personal',
+			items: [
+				{
+					name: 'Accord',
+					description: 'A third party Discord web client',
+					license: 'OSL-3.0',
+					unreleased: true,
+				},
+			],
+		},
+		{
+			header: 'Contributions',
+			items: [
+				{
+					name: 'Powercord',
+					description: 'A lightweight Discord client mod focused on simplicity and performance.',
+					website: 'https://powercord.dev',
+					source: 'https://github.com/powercord-org/powercord',
+				},
+			],
+		},
+		{
+			header: 'Libraries',
+			items: [
+				{
+					name: 'substate',
+					description: 'Simple global state management',
+					license: 'MIT',
+					source: 'https://github.com/intrnl/substate',
+				},
+				{
+					name: 'rodo',
+					description: 'Simple filesystem router',
+					license: 'MIT',
+					source: 'https://github.com/intrnl/rodo',
+				},
+				{
+					name: 'styl',
+					description: 'Lightweight CSS-in-JS runtime',
+					license: 'MIT',
+					source: 'https://github.com/intrnl/styl',
+				},
+				{
+					name: 'styleprops',
+					description: 'CSS custom properties concat helper',
+					license: 'MIT',
+					source: 'https://github.com/intrnl/styleprops',
+				},
+				{
+					name: 'fe',
+					description: 'Glorified fetch wrapper',
+					license: 'MIT',
+					source: 'https://github.com/intrnl/fe',
+				},
+			],
+		},
+		{
+			header: 'Bundler plugins',
+			items: [
+				{
+					name: 'vite-plugin-svelte',
+					description: 'Svelte integration for Vite',
+					license: 'MIT',
+					source: 'https://github.com/intrnl/vite-plugin-svelte',
+				},
+				{
+					name: 'vite-plugin-external-worker',
+					description: 'Vite plugin for external bundling of web workers',
+					license: 'MIT',
+					source: 'https://github.com/intrnl/vite-plugin-external-worker',
+				},
+			],
+		},
+	];
+</script>
+
 <script>
   import DefaultView from '@/components/DefaultView';
   import ExtLink from '@/components/ExtLink';
@@ -10,73 +89,41 @@
 <DefaultView>
   <h1>Projects</h1>
 
-  <h2>Personal</h2>
-  <ul>
-    <li role='article'><header>
-      <h3>Accord</h3>
-      <p>A third party Discord web client</p>
-      <small>OSL-3.0 - Soon.</small>
-    </header></li>
-  </ul>
+	{#each entries as categories}
+		<h2>{categories.header}</h2>
 
-  <h2>Contributed</h2>
-  <ul>
-    <li role='article'><header>
-      <h3>Powercord</h3>
-      <p>A lightweight Discord client mod focused on simplicity and performance.</p>
-      <small>
-        <ExtLink url='https://powercord.dev'>Website</ExtLink> -
-        <ExtLink url='https://github.com/powercord-org/powercord'>Source code</ExtLink>
-      </small>
-    </header></li>
-  </ul>
+		<ul>
+			{#each categories.items as item}
+				<li role='article'><header>
+					<h3>{item.name}</h3>
+					{#if item.description}
+						<p>{item.description}</p>
+					{/if}
 
-  <h2>Libraries and miscellaneous stuff</h2>
-  <ul>
-    <li role='article'><header>
-      <h3>svelte-query</h3>
-      <p>Svelte library for managing, caching and syncing asynchronous and remote data</p>
-      <small>
-        MIT -
-        <ExtLink url='https://npm.im/@intrnl/svelte-query'>npm</ExtLink> -
-        <ExtLink url='https://github.com/intrnl/svelte-query'>Source code</ExtLink>
-      </small>
-    </header></li>
-    <li role='article'><header>
-      <h3>vite-plugin-svelte</h3>
-      <p>Svelte integration for Vite, a fast web dev tool</p>
-      <small>
-        MIT -
-        <ExtLink url='https://npm.im/vite-plugin-svelte'>npm</ExtLink> -
-        <ExtLink url='https://github.com/intrnl/vite-plugin-svelte'>Source code</ExtLink>
-      </small>
-    </header></li>
-    <li role='article'><header>
-      <h3>vite-plugin-external-worker</h3>
-      <p>Vite plugin for non-inline bundling of web workers</p>
-      <small>
-        MIT -
-        <ExtLink url='https://npm.im/vite-plugin-external-worker'>npm</ExtLink> -
-        <ExtLink url='https://github.com/intrnl/vite-plugin-external-worker'>Source code</ExtLink>
-      </small>
-    </header></li>
-    <li role='article'><header>
-      <h3>substate</h3>
-      <p>Simple global state management</p>
-      <small>
-        MIT -
-        <ExtLink url='https://npm.im/@intrnl/substate'>npm</ExtLink> -
-        <ExtLink url='https://github.com/intrnl/substate'>Source code</ExtLink>
-      </small>
-    </header></li>
-    <li role='article'><header>
-      <h3>pako-esm</h3>
-      <p>ESM conversion of pako, a high speed zlib port to JavaScript</p>
-      <small>
-        MIT -
-        <ExtLink url='https://npm.im/@intrnl/pako-esm'>npm</ExtLink> -
-        <ExtLink url='https://github.com/intrnl/pako-esm'>Source code</ExtLink>
-      </small>
-    </header></li>
-  </ul>
+					{#if item.unreleased}
+						<small>Soon.</small>
+					{:else}
+						{#if item.license}
+							<small>{item.license}</small>
+						{/if}
+						{#if item.website}
+							<small>
+								<ExtLink url={item.website}>Website</ExtLink>
+							</small>
+						{/if}
+						{#if item.npm}
+							<small>
+								<ExtLink url={item.npm}>NPM</ExtLink>
+							</small>
+						{/if}
+						{#if item.source}
+							<small>
+								<ExtLink url={item.source}>Source</ExtLink>
+							</small>
+						{/if}
+					{/if}
+				</header></li>
+			{/each}
+		</ul>
+	{/each}
 </DefaultView>
